683a1a6eab28c7df2c7b01d929bbab94e250c3a8
cyberbotx <ill-jah@yandex.ru>
2019-10-06 17:42:26 +0300

    Task Phonebook. v6. Optimized imports. Deleted unnecessary log4j2.xml.

--- src/test/java/phonebook/config/HyperSQLconfig.java
+++ src/test/java/phonebook/config/HyperSQLconfig.java
@@ -6,8 +6,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
